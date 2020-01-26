888074bbb2434f476e0040a89198ce26e980bf8d
Ahmad Majid <Ahmadmajid1994@gmail.com>
2020-01-26 01:56:55 +0000

    routing

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,25 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+    shoes: 1,
+    kids: 2,
+    originals: 3,
+    womens: 4,
+    mens: 5        
+};
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
+);
+
+export const selectCollection = collectionUrlParam =>
+    createSelector(
+        [selectCollections],
+        collections => 
+            collections.find(
+                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+        )
 );
